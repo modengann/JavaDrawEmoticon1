bb4b86b22d1edba4f9f9d6f67b47f823eef0cde3
modengann <99417989+modengann@users.noreply.github.com>
2022-09-18 21:50:52 -0400

    init

---
+++ src/main/java/Main.java
@@ -0,0 +1,30 @@
+import javadraw.*;
+public class Main extends Window{
+    public static void main(String[] args) {
+    /*Task 1
+        
+        Watch YouTube video: https://youtu.be/4j2J55u_sok?list=PLpkMWkvwtatjEEHLbKGp1HJOggcz2O4sM
+        Get a window to open
+        
+    */
+
+    }
+
+    public void start(){
+        /* Task 2:
+            Watch YouTube video: https://youtu.be/nmhCvtNWUe4?list=PLpkMWkvwtatjEEHLbKGp1HJOggcz2O4sM
+            Create four Rectangles of varying sizes and locations
+
+
+        */
+
+
+        /*Task 3:
+            Watch YouTube video: https://youtu.be/QZqB3GQUtMg?list=PLpkMWkvwtatjEEHLbKGp1HJOggcz2O4sM
+            1. Change the color of a rectangle
+            2. Move a rectangle using the .move() command
+            3. Open-ended! Find a way to show me something new you learned in the Documentation (https://javadraw.graphics/api/)
+
+        */
+    }
+}
